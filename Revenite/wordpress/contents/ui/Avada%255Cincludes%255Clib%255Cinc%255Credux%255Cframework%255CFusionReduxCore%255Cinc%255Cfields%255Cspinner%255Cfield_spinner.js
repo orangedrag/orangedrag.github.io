75eaabd8970935ac16476856752a3989eee9fa2d
orangedrag <34955799+orangedrag@users.noreply.github.com>
2018-11-13 20:39:28 -0800

    WP Static HTML Output plugin: 2018-11-14 04:39:30

---
+++ Revenite/wordpress/contents/ui/Avada%255Cincludes%255Clib%255Cinc%255Credux%255Cframework%255CFusionReduxCore%255Cinc%255Cfields%255Cspinner%255Cfield_spinner.js
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html class="avada-html-layout-wide" lang="en-US" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Page not found &#8211; Revenite</title>
+
+
+
+					<link rel="shortcut icon" href="https://orangedrag.github.io/contents/data/2018/11/Revenite_Alt_Black-2.png" type="image/x-icon" />
+		
+		
+		
+		
+				<link rel='stylesheet' id='contact-form-7-css'  href='https://orangedrag.github.io/contents/lib/contact-form-7/includes/css/styles.css' type='text/css' media='all' />
+<link rel='stylesheet' id='bootstrap-css'  href='https://orangedrag.github.io/contents/lib/fusion/includes/bootstrap/front/css/bootstrap.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='fsn_bootstrap-css'  href='https://orangedrag.github.io/contents/lib/fusion/includes/css/fusion-bootstrap.css' type='text/css' media='all' />
+<link rel='stylesheet' id='fsn_core-css'  href='https://orangedrag.github.io/contents/lib/fusion/includes/css/fusion-core.css' type='text/css' media='all' />
+<link rel='stylesheet' id='wpslimclamp-css'  href='https://orangedrag.github.io/contents/lib/wpslimclamp/public/css/wpslimclamp-public.css' type='text/css' media='all' />
+<link rel='stylesheet' id='avada-stylesheet-css'  href='https://orangedrag.github.io/contents/ui/theme/assets/css/style.min.css' type='text/css' media='all' />
+<!--[if lte IE 9]>
+<link rel='stylesheet' id='avada-IE-fontawesome-css'  href='https://orangedrag.github.io/contents/ui/theme/includes/lib/assets/fonts/fontawesome/font-awesome.min.css' type='text/css' media='all' />
+<![endif]-->
+<!--[if IE]>
+<link rel='stylesheet' id='avada-IE-css'  href='https://orangedrag.github.io/contents/ui/theme/assets/css/ie.min.css' type='text/css' media='all' />
+<![endif]-->
+<link rel='stylesheet' id='fusion-dynamic-css-css'  href='https://orangedrag.github.io/contents/data/fusion-styles/dacd2f759ce76c07933a418f27431a1a.min.css' type='text/css' media='all' />
+<script type='text/javascript' src='https://orangedrag.github.io/inc/js/jquery/jquery.js'></script>
+<script type='text/javascript' src='https://orangedrag.github.io/inc/js/jquery/jquery-migrate.min.js'></script>
+<script type='text/javascript' src='https://orangedrag.github.io/contents/lib/fusion/includes/js/modernizr-3.3.1-respond-1.4.2.min.js'></script>
+<script type='text/javascript' src='https://orangedrag.github.io/contents/lib/wpslimclamp/public/js/slimclamp.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var currentClumps = {"clamps":""};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://orangedrag.github.io/contents/lib/wpslimclamp/public/js/wpslimclamp-public.js'></script>
+
+
+
+		<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+		
+		
+	<script type="text/javascript">
+		var doc = document.documentElement;
+		doc.setAttribute('data-useragent', navigator.userAgent);
+	</script>
+
+	</head>
+
+<body class="error404 fusion-image-hovers fusion-body ltr fusion-sticky-header no-tablet-sticky-header no-mobile-sticky-header no-mobile-slidingbar no-mobile-totop fusion-disable-outline mobile-logo-pos-left layout-wide-mode fusion-top-header menu-text-align-center mobile-menu-design-modern fusion-show-pagination-text fusion-header-layout-v3 avada-responsive avada-footer-fx-none fusion-search-form-classic fusion-avatar-square">
+	<a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+				<div id="wrapper" class="">
+		<div id="home" style="position:relative;top:-1px;"></div>
+		
+			<header class="fusion-header-wrapper">
+				<div class="fusion-header-v3 fusion-logo-left fusion-sticky-menu- fusion-sticky-logo- fusion-mobile-logo-  fusion-mobile-menu-design-modern">
+					
+<div class="fusion-secondary-header">
+	<div class="fusion-row">
+							<div class="fusion-alignright">
+				<div class="fusion-social-links-header"><div class="fusion-social-networks"><div class="fusion-social-networks-wrapper"><a  class="fusion-social-network-icon fusion-tooltip fusion-facebook fusion-icon-facebook" style="color:#bebdbd;" href="https://www.facebook.com/Revenite-Inc-2177854312445768/" target="_blank" rel="noopener noreferrer" data-placement="bottom" data-title="Facebook" data-toggle="tooltip" title="Facebook"><span class="screen-reader-text">Facebook</span></a><a  class="fusion-social-network-icon fusion-tooltip fusion-linkedin fusion-icon-linkedin" style="color:#bebdbd;" href="https://www.linkedin.com/company/revenite-inc/" target="_blank" rel="noopener noreferrer" data-placement="bottom" data-title="LinkedIn" data-toggle="tooltip" title="LinkedIn"><span class="screen-reader-text">LinkedIn</span></a></div></div></div>			</div>
+			</div>
+</div>
+<div class="fusion-header-sticky-height"></div>
+<div class="fusion-header">
+	<div class="fusion-row">
+					<div class="fusion-logo" data-margin-top="10px" data-margin-bottom="10px" data-margin-left="0px" data-margin-right="0px">
+			<a class="fusion-logo-link"  href="https://orangedrag.github.io/" >
+
+						<!-- standard logo -->
+			<img src="https://orangedrag.github.io/contents/data/2018/10/Revenite_Black-e1540718470335.png" srcset="https://orangedrag.github.io/contents/data/2018/10/Revenite_Black-e1540718470335.png 1x, https://orangedrag.github.io/contents/data/2018/10/Revenite_Black-1-e1540721256197.png 2x" width="260" height="64" style="max-height:64px;height:auto;" alt="Revenite Logo" retina_logo_url="https://orangedrag.github.io/contents/data/2018/10/Revenite_Black-1-e1540721256197.png" class="fusion-standard-logo" />
+
+			
+					</a>
+		</div>		<nav class="fusion-main-menu" aria-label="Main Menu"><ul id="menu-top-menu" class="fusion-menu"><li  id="menu-item-1034"  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1034 fusion-menu-item-button"  data-item-id="1034"><a  href="https://orangedrag.github.io/contact-2/" class="fusion-background-highlight" role="menuitem"><span class="menu-text fusion-button button-default button-large">Contact</span></a></li></ul></nav>	<div class="fusion-mobile-menu-icons">
+							<a href="#" class="fusion-icon fusion-icon-bars" aria-label="Toggle mobile menu" aria-expanded="false"></a>
+		
+		
+		
+			</div>
+	
+<nav class="fusion-mobile-nav-holder fusion-mobile-menu-text-align-left" aria-label="Main Menu Mobile"></nav>
+
+					</div>
+</div>
+				</div>
+				<div class="fusion-clearfix"></div>
+			</header>
+					
+		
+		<div id="sliders-container">
+					</div>
+				
+		
+					<div class="fusion-page-title-bar fusion-page-title-bar-breadcrumbs fusion-page-title-bar-center">
+	<div class="fusion-page-title-row">
+		<div class="fusion-page-title-wrapper">
+			<div class="fusion-page-title-captions">
+
+																			<h1 class="entry-title">Error 404 Page</h1>
+
+									
+													
+			</div>
+
+			
+		</div>
+	</div>
+</div>
+				
+				<main id="main" role="main" class="clearfix " style="">
+			<div class="fusion-row" style="">
+<section id="content" class="full-width">
+	<div id="post-404page">
+		<div class="post-content">
+						<div class="fusion-title fusion-title-size-two sep-none fusion-sep-none" style="margin-top:0px;margin-bottom:30px;">
+				<h2 class="title-heading-left">
+					Oops, This Page Could Not Be Found!				</h2>
+			</div>
+						<div class="fusion-clearfix"></div>
+			<div class="error-page">
+				<div class="fusion-columns fusion-columns-3">
+					<div class="fusion-column col-lg-4 col-md-4 col-sm-4 fusion-error-page-404">
+						<div class="error-message">404</div>
+					</div>
+					<div class="fusion-column col-lg-4 col-md-4 col-sm-4 useful-links fusion-error-page-useful-links">
+						<h3>Helpful Links</h3>
+						<ul id="fusion-checklist-1" class="fusion-checklist fusion-404-checklist error-menu" style="font-size:14px;line-height:23.8px;"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1034"><span class="icon-wrapper circle-no" style="font-size:14px;height:23.8px;width:23.8px;margin-right:9.8px;" ><i class="fusion-li-icon fa fa-angle-right" style="color:rgba(28,214,195,0.83);"></i></span><div class="fusion-li-item-content" style="margin-left:leftpx;"><a href="https://orangedrag.github.io/contact-2/">Contact</a></div></li></ul>					</div>
+					<div class="fusion-column col-lg-4 col-md-4 col-sm-4 fusion-error-page-search">
+						<h3>Search Our Website</h3>
+						<p>Can&#039;t find what you need? Take a moment and do a search below!</p>
+						<div class="search-page-search-form">
+							<form role="search" class="searchform fusion-search-form" method="get" action="https://orangedrag.github.io/">
+	<div class="fusion-search-form-content">
+		<div class="fusion-search-field search-field">
+			<label class="screen-reader-text" for="s">Search for:</label>
+			<input type="text" value="" name="s" class="s" placeholder="Search ..." required aria-required="true" aria-label="Search ..."/>
+		</div>
+		<div class="fusion-search-button search-button">
+			<input type="submit" class="fusion-search-submit searchsubmit" value="&#xf002;" />
+		</div>
+	</div>
+</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+					
+				</div>  <!-- fusion-row -->
+			</main>  <!-- #main -->
+			
+			
+			
+										
+				<div class="fusion-footer">
+					
+	
+	<footer id="footer" class="fusion-footer-copyright-area">
+		<div class="fusion-row">
+			<div class="fusion-copyright-content">
+
+				<div class="fusion-copyright-notice">
+		<div>
+		© Copyright 2016 - <script>document.write(new Date().getFullYear());</script>   |   <a href='http://Reveniteinc.com' target='_blank'>Revenite Inc.</a>   |   All Rights Reserved   	</div>
+</div>
+<div class="fusion-social-links-footer">
+	<div class="fusion-social-networks boxed-icons"><div class="fusion-social-networks-wrapper"><a  class="fusion-social-network-icon fusion-tooltip fusion-facebook fusion-icon-facebook" style="color:#ffffff;background-color:#5bdccd;border-color:#5bdccd;border-radius:50%;" href="https://www.facebook.com/Revenite-Inc-2177854312445768/" target="_blank" rel="noopener noreferrer" data-placement="top" data-title="Facebook" data-toggle="tooltip" title="Facebook"><span class="screen-reader-text">Facebook</span></a><a  class="fusion-social-network-icon fusion-tooltip fusion-linkedin fusion-icon-linkedin" style="color:#ffffff;background-color:#5bdccd;border-color:#5bdccd;border-radius:50%;" href="https://www.linkedin.com/company/revenite-inc/" target="_blank" rel="noopener noreferrer" data-placement="top" data-title="LinkedIn" data-toggle="tooltip" title="LinkedIn"><span class="screen-reader-text">LinkedIn</span></a></div></div></div>
+
+			</div> <!-- fusion-fusion-copyright-content -->
+		</div> <!-- fusion-row -->
+	</footer> <!-- #footer -->
+				</div> <!-- fusion-footer -->
+			
+								</div> <!-- wrapper -->
+
+								<a class="fusion-one-page-text-link fusion-page-load-link"></a>
+
+						<script type="text/javascript">
+				jQuery( document ).ready( function() {
+					var ajaxurl = 'https://orangedrag.github.io/wp-admin/admin-ajax.php';
+					if ( 0 < jQuery( '.fusion-login-nonce' ).length ) {
+						jQuery.get( ajaxurl, { 'action': 'fusion_login_nonce' }, function( response ) {
+							jQuery( '.fusion-login-nonce' ).html( response );
+						});
+					}
+				});
+				</script>
+				<style></style><script type='text/javascript'>
+/* <![CDATA[ */
+var wpcf7 = {"apiSettings":{"root":"http:\/\/lewis.myddns.com:8081\/Revenite\/wordpress\/wp-json\/contact-form-7\/v1","namespace":"contact-form-7\/v1"},"recaptcha":{"messages":{"empty":"Please verify that you are not a robot."}}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://orangedrag.github.io/contents/lib/contact-form-7/includes/js/scripts.js'></script>
+<script type='text/javascript' src='https://orangedrag.github.io/contents/lib/fusion/includes/bootstrap/front/js/bootstrap.min.js'></script>
+<script type='text/javascript' src='https://orangedrag.github.io/contents/lib/fusion/includes/utilities/imagesloaded/imagesloaded.pkgd.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var fsnAjax = {"ajaxurl":"http:\/\/lewis.myddns.com:8081\/Revenite\/wordpress\/wp-admin\/admin-ajax.php","pluginurl":"http:\/\/lewis.myddns.com:8081\/Revenite\/wordpress\/contents\/lib\/fusion\/"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://orangedrag.github.io/contents/lib/fusion/includes/js/fusion-core.js'></script>
+<!--[if IE 9]>
+<script type='text/javascript' src='https://orangedrag.github.io/contents/ui/theme/includes/lib/assets/min/js/general/fusion-ie9.js'></script>
+<![endif]-->
+<script type='text/javascript' src='https://orangedrag.github.io/contents/data/fusion-scripts/f9b7bc1a964cb35a89cbcb31c3312b6d.min.js'></script>
+	</body>
+</html>
